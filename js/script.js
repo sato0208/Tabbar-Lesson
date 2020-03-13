654faeca94b3cc938cdc6a039316506d34c49041
里千晴 <satochiharu@satonoMacBook-Pro.local>
2020-03-14 00:43:23 +0900

    [Finish]タブメニューの作成

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
+
+// id="tab-contentsの要素のうちclass=tab,id="tab1"でない要素を非表示にする
 $('#tab-contents .tab[id != "tab1"]').hide();
 
 $('#tab-menu a').on('click', function(){
+// タブコンテンツの中のaタグをクリックした時に非表示にする
+	// tab-contents内のタブがクリックされたらそのタブないのコンテンツを非表示にする
 	$("#tab-contents .tab").hide();
+	// タブを押した時に色を変えるためにremoveclassでクラスを削除する
 	$("tab-menu .active").removeClass("active");
+	// thisつまりタブがクリックされた時にactiveクラスを追加する
 	$(this).addClass("active");
+	// .attrはHTML要素の属性を取得したり設定できる
 	$($(this).attr("href")).show();
 	return false;
 });
